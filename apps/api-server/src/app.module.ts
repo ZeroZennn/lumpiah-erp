3e202f5d3b181183a6c675a3c6e6ef8272036ca1
ZeroZennn <achmad.zikran@gmail.com>
2026-01-28 15:19:50 +0700

    Initial Commit

---
+++ apps/api-server/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PosModule } from './modules/pos/pos.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { ReportsModule } from './modules/reports/reports.module';
+import { BranchesModule } from './modules/branches/branches.module';
+import { DssEngineModule } from './modules/dss-engine/dss-engine.module';
+import { ProductionModule } from './modules/production/production.module';
+import { HrModule } from './modules/hr/hr.module';
+import { UsersModule } from './modules/users/users.module';
+import { SystemModule } from './modules/system/system.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [PosModule, InventoryModule, ReportsModule, BranchesModule, DssEngineModule, ProductionModule, HrModule, UsersModule, SystemModule, CommonModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
