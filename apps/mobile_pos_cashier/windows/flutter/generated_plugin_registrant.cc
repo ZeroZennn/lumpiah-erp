3279e8116356df8d86901c89b010413fc630db4e
ZeroZennn <achmad.zikran@gmail.com>
2026-01-28 15:57:25 +0700

    install depedencies

--- apps/mobile_pos_cashier/windows/flutter/generated_plugin_registrant.cc
+++ apps/mobile_pos_cashier/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_blue_plus_winrt/flutter_blue_plus_plugin.h>
+#include <isar_flutter_libs/isar_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterBluePlusPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterBluePlusPlugin"));
+  IsarFlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("IsarFlutterLibsPlugin"));
 }
